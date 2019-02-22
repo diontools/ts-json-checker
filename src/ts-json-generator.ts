a19bdc8d67e8174db4fb11e308bf46af6ad1d05e
diontools <diontools@users.noreply.github.com>
2019-02-22 14:39:28 +0900

    colors

--- src/ts-json-generator.ts
+++ src/ts-json-generator.ts
@@ -1,23 +1,7 @@
 import * as ts from 'typescript'
 import * as fs from 'fs'
 import * as path from 'path'
-
-const Reset = "\x1b[0m";
-const Bright = "\x1b[1m";
-const Dim = "\x1b[2m";
-const Underscore = "\x1b[4m";
-const Blink = "\x1b[5m";
-const Reverse = "\x1b[7m";
-const Hidden = "\x1b[8m";
-
-const FgBlack = "\x1b[30m";
-const FgRed = "\x1b[31m";
-const FgGreen = "\x1b[32m";
-const FgYellow = "\x1b[33m";
-const FgBlue = "\x1b[34m";
-const FgMagenta = "\x1b[35m";
-const FgCyan = "\x1b[36m";
-const FgWhite = "\x1b[37m";
+import { FgWhite, Reset, Bright, FgYellow, FgCyan, FgGreen, FgMagenta } from './Colors';
 
 const DEBUG = true
 if (!DEBUG) {
