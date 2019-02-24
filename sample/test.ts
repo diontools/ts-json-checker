20a6894ed15c667e1bf3dc8a7d94d08bd78797be
diontools <diontools@users.noreply.github.com>
2019-02-25 00:04:39 +0900

    move test

--- sample/test.ts
+++ sample/test.ts
@@ -1,4 +1,4 @@
-import { parseT, parseX } from "./sample/generated";
+import { parseT, parseX } from "./generated";
 
 let json = JSON.parse('{ "n": 1, "s": "a", "b": true, "u": null, "x": { "n": 1 } }');
 let v = parseT(json);
