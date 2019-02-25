6364918fcc87312d9cff586c92842e12b97c08b6
diontools <diontools@users.noreply.github.com>
2019-02-25 11:32:58 +0900

    basic test with jest

---
+++ sample/generated.test.ts
@@ -0,0 +1,46 @@
+import * as g from "./generated";
+
+enum BasicType {
+    number,
+    string,
+    boolean,
+    object,
+    undefined,
+    null,
+}
+
+const basicFuncs = [
+    { type: BasicType.number, func: g.parseN },
+    { type: BasicType.string, func: g.parseS },
+    { type: BasicType.boolean, func: g.parseB },
+    { type: BasicType.object, func: g.parseO },
+    { type: BasicType.undefined, func: g.parseD },
+    { type: BasicType.null, func: g.parseU },
+]
+
+const inputPatterns = [
+    { type: BasicType.number, value: 1 },
+    { type: BasicType.string, value: 'a' },
+    { type: BasicType.boolean, value: true },
+    { type: BasicType.object, value: {} },
+    { type: BasicType.undefined, value: undefined },
+    { type: BasicType.null, value: null },
+]
+
+for (const bf of basicFuncs) {
+    for (const p of inputPatterns) {
+        if (p.type === bf.type) {
+            test(BasicType[bf.type] + ' with ' + BasicType[p.type], () => expect(bf.func(p.value)).toBe(p.value))
+        } else {
+            test(BasicType[bf.type] + ' with ' + BasicType[p.type], () => expect(() => bf.func(p.value)).toThrow())
+        }
+    }
+}
+
+//test("number", () => expect(g.parseN(0)).toBe(0))
+//test("not number", () => expect(() => g.parseN('a')).toThrow())
+
+// let json = JSON.parse('{ "n": 1, "s": "a", "b": true, "u": null, "x": { "n": 1 } }');
+// let v = parseT(json);
+
+// let r = parseX({});
--- sample/test.ts
+++
@@ -1,6 +0,0 @@
-import { parseT, parseX } from "./generated";
-
-let json = JSON.parse('{ "n": 1, "s": "a", "b": true, "u": null, "x": { "n": 1 } }');
-let v = parseT(json);
-
-let r = parseX({});
